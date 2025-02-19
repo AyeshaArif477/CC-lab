89b930deb651de6b4d0989aac3da3fde1c20b6b8
AyeshaArif477 <siriufugth@gmail.com>
2025-02-19 15:00:56 +0500

    file1.cs

--- file1.cs
+++ file1.cs
@@ -10,7 +10,7 @@ public class PasswordChecker
 
         // Define the pattern with the appropriate lookahead checks
         string pattern = @"^(?=.*[A-Z])         " +  // At least 1 uppercase
-                         @"(?=.[!@#$%^&])     " +  // At least 1 special character
+                         @"(?=.[!@#$%^&].*[!@#$%^&*])     " +  // At least 1 special character
                          @"(?=.\d.\d)         " +  // At least 2 digits
                          @"(?=.[a-z].[a-z].[a-z].[a-z])" +  // At least 4 lowercase letters
                          @"[A-Za-z0-9!@#$%^&*]{1,12}$";         // Up to 12 chars total
